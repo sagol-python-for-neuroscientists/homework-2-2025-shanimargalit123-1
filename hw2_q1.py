569f80f73c87b8306607e5bff42b0411e433bbe2
Shani Margalit Bram <shanimargalitbram@Shanis-MacBook-Air.local>
2026-05-17 17:41:02 +0300

    homework assignment 2 submission

--- hw2_q1.py
+++ hw2_q1.py
@@ -1,26 +1,50 @@
-MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
-              'D': '-..',    'E': '.',      'F': '..-.',
-              'G': '--.',    'H': '....',   'I': '..',
-              'J': '.---',   'K': '-.-',    'L': '.-..',
-              'M': '--',     'N': '-.',     'O': '---',
-              'P': '.--.',   'Q': '--.-',   'R': '.-.',
-              'S': '...',    'T': '-',      'U': '..-',
-              'V': '...-',   'W': '.--',    'X': '-..-',
-              'Y': '-.--',   'Z': '--..',
-
-              '0': '-----',  '1': '.----',  '2': '..---',
-              '3': '...--',  '4': '....-',  '5': '.....',
-              '6': '-....',  '7': '--...',  '8': '---..',
-              '9': '----.',
-
-              '.': '.-.-.-', ',': '--..--', ':': '---...',
-              "'": '.----.', '-': '-....-',
-              }
+MORSE_CODE = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    ":": "---...",
+    "'": ".----.",
+    "-": "-....-",
+}
 
 
 def english_to_morse(
-    input_file: str = "lorem.txt",
-    output_file: str = "lorem_morse.txt"
+    input_file: str = "lorem.txt", output_file: str = "lorem_morse.txt"
 ):
     """Convert an input text file to an output Morse code file.
 
@@ -37,3 +61,32 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+
+    # We store the final results here
+    translated_content = []
+
+    with open(input_file, "r") as file1:
+        # We read each line from the source
+        for line in file1:
+            line_result = []
+            for char in line.upper():
+                if char in MORSE_CODE:
+                    line_result.append(MORSE_CODE[char])
+                elif char == " " or char == "\n":
+                    # Per your requirement: spaces go to a new line
+                    line_result.append("\n")
+
+            # Join characters with a space for clarity, then add to our list
+            translated_content.append("".join(line_result))
+
+    # Join everything into one big string to write
+    final_output = "".join(translated_content)
+
+    with open(output_file, "w") as file2:
+        file2.write(final_output)
+
+    return final_output
+
+
+if __name__ == "__main__":
+    english_to_morse()
